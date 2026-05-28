28ed6b5252679724c1c1025bb3f055a189f0ae40
itadmin <ypisal156@gmail.com>
2026-05-28 15:32:01 +0530

    deployinf green env..

--- app/app.py
+++ app/app.py
@@ -16,8 +16,7 @@ redis_client = redis.Redis(
 @app.route("/")
 def home():
     return jsonify({
-        "message": "Flask Redis app is running on blue version...",
-        #"message": "Flask Redis app is running on green version...",
+        "message": "Flask Redis app is running...",
         "redis_host": REDIS_HOST
     })
 
